4d4685f1c13668412d129bd28bc6f5a039c5efbe
ilink <ian@linkportfolio.net>
2012-11-26 02:10:25 -0800

    blending with lazers

--- public/js/shaders/rttShaderFs.glsl
+++ public/js/shaders/rttShaderFs.glsl
@@ -5,24 +5,40 @@
 	uniform sampler2D uSampler;
 
 	void main(void) {
-	    vec4 sample0,
-	    	 sample1,
-	   		 sample2,
-	    	 sample3;
+	    // vec4 sample0,
+	    // 	 sample1,
+	   	// 	 sample2,
+	    // 	 sample3;
 
-	    float step = 0.5 / 100.0;
-	    sample0 = texture2D(uSampler,
-	    	vec2(vTextureCoord.x - step,
-	    		vTextureCoord.y - step));
-	    sample1 = texture2D(uSampler,
-	    	vec2(vTextureCoord.x + step,
-	    		vTextureCoord.y + step));
-	    sample2 = texture2D(uSampler,
-	    	vec2(vTextureCoord.x + step,
-	    		vTextureCoord.y - step));
-	    sample3 = texture2D(uSampler,
-	    	vec2(vTextureCoord.x - step,
-	    		vTextureCoord.y + step));
-	    gl_FragColor = (sample0 + sample1 + sample2 + sample3) / 4.0; 
+	    // float step = 0.5 / 100.0;
+	    // sample0 = texture2D(uSampler,
+	    // 	vec2(vTextureCoord.x - step,
+	    // 		vTextureCoord.y - step));
+	    // sample1 = texture2D(uSampler,
+	    // 	vec2(vTextureCoord.x + step,
+	    // 		vTextureCoord.y + step));
+	    // sample2 = texture2D(uSampler,
+	    // 	vec2(vTextureCoord.x + step,
+	    // 		vTextureCoord.y - step));
+	    // sample3 = texture2D(uSampler,
+	    // 	vec2(vTextureCoord.x - step,
+	    // 		vTextureCoord.y + step));
+	    // gl_FragColor = (sample0 + sample1 + sample2 + sample3) / 4.0;
+
+	    vec4 sum = vec4(0.0);
+	    float blurSize = 0.005;
+	    sum += texture2D(uSampler, vTextureCoord - 4.0 * blurSize) * 0.05;
+	    sum += texture2D(uSampler, vTextureCoord - 3.0 * blurSize) * 0.09;
+	    sum += texture2D(uSampler, vTextureCoord - 2.0 * blurSize) * 0.12;
+	    sum += texture2D(uSampler, vTextureCoord - 1.0 * blurSize) * 0.15;
+	    sum += texture2D(uSampler, vTextureCoord                 ) * 0.16;
+	    sum += texture2D(uSampler, vTextureCoord + 1.0 * blurSize) * 0.15;
+	    sum += texture2D(uSampler, vTextureCoord + 2.0 * blurSize) * 0.12;
+	    sum += texture2D(uSampler, vTextureCoord + 3.0 * blurSize) * 0.09;
+	    sum += texture2D(uSampler, vTextureCoord + 4.0 * blurSize) * 0.05;
+	    gl_FragColor = sum;
 	}
 </script>
+
+
+
