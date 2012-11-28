d65a2fc522970cb05cb10cd3ace49dd9299bf3e8
ilink <ian@linkportfolio.net>
2012-11-28 02:02:07 -0800

    using fog

--- public/js/shaders/laser_fs.glsl
+++ public/js/shaders/laser_fs.glsl
@@ -9,6 +9,7 @@
 	uniform vec2 resolution;
 	varying vec2 vertColorOut;  // these happen to also be texture coordinates!
 	uniform sampler2D uSampler; 
+	const float fog_density = 0.05;
 
 	void main( void ) {
 		vec2 position = vertColorOut.xy;
@@ -19,7 +20,32 @@
 
 		vec4 sample = texture2D(uSampler, vec2(vertColorOut.s, vertColorOut.t));
 		sample[3] = alpha;
-		gl_FragColor = sample;
+
+		// Fog
+		const float LOG2 = 20.442695;
+		// Is the W coord related to the transform part?
+		float z = gl_FragCoord.z / gl_FragCoord.w;
+
+		// -fogdensity^2 * z^2 * log(2) => why?
+		float fogFactor = exp2( -fog_density * 
+						   fog_density * 
+						   z * 
+						   z * 
+						   LOG2 );
+		fogFactor = clamp(fogFactor, 0.0, 1.0);
+
+		vec4 fog_color = vec4(1,1,1,1.0);
+		vec2 _position = -1.0 + gl_FragCoord.xy / resolution.xy;
+		red = abs( sin( _position.y * _position.y / 5.0 ) );
+		green = abs( sin( _position.y * _position.y / 4.0 ) );
+		blue = abs( sin( _position.y * _position.y / 3.0 ) );
+
+		vec4 final_color;
+		// final_color.rgba = vec4(red, gree, blue, 1.0);
+		final_color.rgba = vec4(0.0, 0.3, 1.0, 1.0);
+		final_color.rgba = vec4(1.0, 0.3, 1.0, 1.0);
+		// gl_FragColor = mix(fog_color, final_color, fogFactor );
+		gl_FragColor = mix(fog_color, sample, fogFactor);
 	}
 
 </script>
