837d9bf2a4e41075187eec3c3fdd070a20e46c79
ilink <ian@linkportfolio.net>
2012-12-08 23:32:12 -0800

    base case

--- public/js/glCore/normals.js
+++ public/js/glCore/normals.js
@@ -5,22 +5,28 @@ function calcFaceNormals(verts){
 	var normal = vec3.create();
 
 	for(var i = 0; i < verts.length; i+=3){ // i think its +6...devil is in the off by 3 errors too, apparently
-		if(i % 2){
-			// i recommend looking at a picture of a triangle strip if this is confusing
-			// basically we have to treat the first as a full triangle and the rest as an extension of it
-			// or do we treat them differently based upon even and odd?
-
-			/*
-			first case is special
-			then reverse next/prev on even/odd
-			*/
-			vec1.set(verts[i], verts[i+1], verts[i+2]);
-			vec2.set(verts[i-3], verts[i-2], verts[i-1]);
-			vec3.set(verts[i+3], verts[i+4], verts[i+5]);	
-		} else {
+		if(i === 0){
 			vec1.set(verts[i], verts[i+1], verts[i+2]);
 			vec2.set(verts[i+3], verts[i+4], verts[i+5]);
 			vec3.set(verts[i+6], verts[i+7], verts[i+8]);
+		} else {
+			if(i % 2){
+				// i recommend looking at a picture of a triangle strip if this is confusing
+				// basically we have to treat the first as a full triangle and the rest as an extension of it
+				// or do we treat them differently based upon even and odd?
+
+				/*
+				first case is special
+				then reverse next/prev on even/odd
+				*/
+				vec1.set(verts[i], verts[i+1], verts[i+2]);
+				vec2.set(verts[i-3], verts[i-2], verts[i-1]);
+				vec3.set(verts[i+3], verts[i+4], verts[i+5]);	
+			} else {
+				vec1.set(verts[i], verts[i+1], verts[i+2]);
+				vec2.set(verts[i+3], verts[i+4], verts[i+5]);
+				vec3.set(verts[i+6], verts[i+7], verts[i+8]);
+			}
 		}
 	}
 }
