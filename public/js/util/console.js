d389a62bb53c6e74d4362287c39f9c7b2ed9aadc
Ian Link <ian@linkportfolio.net>
2012-12-11 17:15:40 -0800

    new thingses

--- public/js/util/console.js
+++ public/js/util/console.js
@@ -3,12 +3,26 @@ function Logger(){
 
 }
 
+Logger.prototype.evalIsEvil = function(){
+
+}
+
 Logger.prototype.log = function(){
 	if(this.level[0].match(/d|w|e/) === null){
-		window.console.log();
+		var evalStr = 'console.log(';
+		var args = Array.prototype.slice.call(arguments);
+		for(var i = 0; i < args.length; i++){
+			var str = arguments[i];
+			if(typeof str === 'string') str = "'"+str+"'";
+			evalStr += str+',';
+		}
+		evalStr = evalStr.slice(0, evalStr.length-1) + ');';
+		console.log(evalStr);
+		eval(evalStr);
+
 	} else return false;
 }
 
 console.log('a');
 var logger = new Logger();
-logger.log('a');
+logger.log('a', {a:1},2,3);
