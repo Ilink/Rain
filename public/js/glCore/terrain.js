e10b51920a2e799dd0c10620089be77a75222ac0
ilink <ian@linkportfolio.net>
2012-12-25 20:45:13 -0800

    kind of fixing off by three error

--- public/js/glCore/terrain.js
+++ public/js/glCore/terrain.js
@@ -25,18 +25,34 @@ function Terrain(params){
 	for(j=0; j < yDim; j++){
 		for(i=0; i < xDim; i++){
 			h = noise.noise(i,j);
-			map.push(h,j,i);
+			map.push(j,h,i);
 		}
 	}
 
-	console.log(map);
 	// this transforms the map verts into a set of faces, all next to one another (in the array)
-	// the output of this function is the format expected by createIndexes
+	function transformFaces(verts, xDim, yDim){
+		var faces = [];
+		var rowCount = 0;
+		for(var i = 0; i < verts.length/3; i+=12){
+			rowCount++;
+			// if we're on the last row, we're done
+			if(i >= verts.length - xDim-2) return;
+			if(rowCount === xDim){
+				rowCount = 0;				
+			} else {
+				faces.push(verts[i], verts[i+1],verts[i+2], 
+						   verts[i+xDim], verts[i+xDim+3], verts[i+xDim+2], 
+						   verts[i+xDim+3], verts[i+xDim+4], verts[i+xDim+5], 
+						   verts[i+3], verts[i+4], verts[i+5]);
+			}
+		}
+		return faces;
+	}
+
+	console.log(map);
 	var faces = [];
-	faceIter(map, xDim, yDim, function(a,b,c,d){
-		faces.push(a,b,c,d);
-	});
-	console.log(faces.length, map.length);
+	faces = transformFaces(map, xDim, yDim)
+	console.log('faces length',faces.length, 'noise map length',map.length);
 	console.log(faces);
 
 	// face indices for drawing
@@ -45,20 +61,6 @@ function Terrain(params){
 	for(var i = 0; i < faces.length/3.0; i+=4){
 		faceIndexes.push(i,i+1,i+2);
 		faceIndexes.push(i,i+2,i+3);
-	}
-
-	function faceIter(verts, xDim, yDim, cb){
-		var rowCount = 0;
-		for(var i = 0; i < verts.length/3; i++){
-			rowCount++;
-			// if we're on the last row, we're done
-			if(i >= verts.length - xDim-2) return;
-			if(rowCount === xDim){
-				rowCount = 0;				
-			} else {
-				cb(verts[i], verts[i+xDim], verts[i+xDim+1], verts[i+1]);
-			}
-		}
 	}
 
 	this.faces = faces;
