8716b8b8c29c899d217b426225129bcafe1ce45b
ilink <ian@linkportfolio.net>
2013-01-12 12:03:38 -0800

    sphere sphere intersection test ideas

---
+++ public/js/core/intersection.js
@@ -0,0 +1,16 @@
+function sphereSphere(aRadius, bRadius, ax, ay, az, bx, by, bz){
+	/*
+	if the radiuses are the same:
+		vector = b - a
+		get vector length
+		if length < radius, they collided
+
+	if the radiuses are different..
+
+	*/
+	if(length < aRadius + bRadius)
+		return true
+	else 
+		return false;
+}
+
